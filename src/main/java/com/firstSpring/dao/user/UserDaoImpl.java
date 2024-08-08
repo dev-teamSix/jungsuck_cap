c6dfe4d1bacda0a1410abdc9dc110e7a0524ea8b
KimGyungSik <0711kyungh@naver.com>
2024-08-08 23:27:36 +0900

    feature : UserDaoTest 작성

--- src/main/java/com/firstSpring/dao/user/UserDaoImpl.java
+++ src/main/java/com/firstSpring/dao/user/UserDaoImpl.java
@@ -5,7 +5,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +12,8 @@ import java.util.Map;
 @Repository
 public class UserDaoImpl implements UserDao {
 
-    @Autowired SqlSession session;
+    @Autowired
+    private SqlSession session;
 
     private static final String namespace = "com.firstSpring.dao.user.UserDao.";
 
@@ -30,119 +30,109 @@ public class UserDaoImpl implements UserDao {
     }
 
 
-    // 회원가입한 고객의 수
+    // 회원가입한 전체 고객의 수 (모든 상태의 회원)
     @Override
     public int countUser() throws Exception {
         return session.selectOne(namespace + "countUser");
     }
 
-    // 가입 상태의 고객의 수
+    // 활성화('O') 상태인 전체 고객의 수
     @Override
     public int countActiveUser() throws Exception {
         return session.selectOne(namespace + "countActiveUser");
     }
 
-    // 회원가입한 전체 고객 조회
+    // 회원가입한 전체 고객 정보 조회 (최근 가입한 고객부터 나열)
     @Override
     public List<UserDto> selectUserAll() throws Exception {
         return session.selectList(namespace + "selectUserAll");
     }
 
-    // 탈퇴 회원을 제외한, 가입상태 'Y' 인 모든 고객 조회
+    // 탈퇴 회원을 제외한, 활성화('O') 상태인 모든 고객 정보 조회 (최근 가입한 고객부터 나열)
     @Override
     public List<UserDto> selectActiveUserAll() throws Exception {
-        return session.selectList(namespace + "selectUserAll");
+        return session.selectList(namespace + "selectActiveUserAll");
     }
 
-    // SELECT
-    // 1. 로그인
-    // 1.1. 아이디로 식별된 특정 고객 조회
-    // 1.1.1. view 에서 입력한 id 를 매개변수로 받고 UserDto 인스턴스에 고객 정보 담아서 반환
-    @Override
-    public UserDto selectUser(String id) throws Exception {
-        return session.selectOne(namespace + "selectUser", id);
-    }
-
-    // INSERT
-    // 2. 회원가입
-    // 2.1. 아이디, 비밀번호, 이름, 생년월일, 성별, 전화번호, 휴대폰 번호, 이메일, 가입일, 가입상태, 최초등록일, 최초등록자, 최종수정일, 최종수정자 데이터 추가
-    // 2.1.2. view 에서 입력한 고객 정보를 UserDto 객체로 받아서 처리하고, 따로 특정 반환 사항 없이 실행 됐으면 1 반환
-    @Override
-    public int insertUser(UserDto userDto) throws Exception {
-        return session.insert(namespace + "insertUser", userDto);
-    }
-
-    // 2.2. 전체 가입고객(탈퇴회원 포함) 중 특정 아이디를 가진 고객 존재여부 확인
+    // 전체 가입고객(탈퇴회원 포함) 중 특정 아이디를 가진 고객 정보 확인
     @Override
     public UserDto selectAllUserId(String id) throws Exception {
         return session.selectOne(namespace + "selectAllUserId", id);
     }
 
-    // UPDATE
-    // 3. 이메일 인증
-    // 3.1. 이메일 인증번호 받을 때마다 mail_key 컬럼에 저장
-    // 3.1.1. 아이디/비밀번호 찾기 본인 인증 시 사용
+    // Login (SELECT)
+    // > 회원상태가 활성화('O') & 메일 인증 여부가 'Y' & 특정 id를 가진 회원
+    @Override
+    public UserDto selectUser(String id) throws Exception {
+        return session.selectOne(namespace + "selectUser", id);
+    }
+
+    // Sign Up (INSERT)
+    // > 아이디, 이메일, 비밀번호, 이름, 생년월일, 성별, 폰번호, 우편번호, 도로명주소, 지번주소, 상세주소, 메일인증키, 시스템컬럼을 insert
+    // > 회원상태 활성화('O') & 메일인증 여부('Y')
+    @Override
+    public int insertUser(UserDto userDto) throws Exception {
+        return session.insert(namespace + "insertUser", userDto);
+    }
+
+
+    // 이메일 인증 (UPDATE)
+    // > 이메일 인증번호 받을 때마다 mail_key 컬럼에 저장
+    // > 아이디/비밀번호 찾기 본인 인증 시 사용
     @Override
     public int updateMailKey(UserDto userDto) throws Exception {
         return session.update(namespace + "updateMailKey", userDto);
     }
 
-    // 4. 아이디/비밀번호 찾기
 
-    // SELECT
-    // 4.1. 아이디 찾기
-    // 4.1.1. 이름 및 이메일로 식별된 특정 회원의 아이디 조회
+    // 아이디 찾기 (SELECT)
+    // > 이름 및 이메일로 식별된 특정 회원의 아이디 조회
     @Override
     public UserDto selectUserId(UserDto userDto) throws Exception {
         return session.selectOne(namespace + "selectUserId", userDto);
     }
 
-    // UPDATE
-    // 4.2. 비밀번호 찾기
-    // 4.2.1. 비밀번호 찾기 시 새로운 비밀번호로 변경
+    // 비밀번호 찾기 (UPDATE)
+    // > 비밀번호 찾기 시 새로운 비밀번호로 변경
     @Override
     public int updateUserPwd(String id, String pwd) throws Exception {
         Map<String, String> map = new HashMap<>();
-        map.put("id", id);
-        map.put("pwd", pwd);
+        map.put("id", id); // 변경될 회원 ID
+        map.put("pwd", pwd); // 변경할 비밀번호
         return session.update(namespace + "updateUserPwd", map);
     }
 
-    // UPDATE
-    // 5. 회원정보 변경
-    // 5.1. 이메일 변경
+    // 이메일 변경 (UPDATE)
     @Override
     public int updateUserEmail(String id, String email) throws Exception {
         Map<String, String> map = new HashMap<>();
-        map.put("id", id);
-        map.put("email", email);
+        map.put("id", id); // 변경될 회원 ID
+        map.put("email", email); // 변경할 이메일
         return session.update(namespace + "updateUserEmail", map);
     }
 
-    // 5.2. 생년월일 변경
+    // 생년월일 변경 (UPDATE)
     @Override
-    public int updateUserBefBirth(String id, Integer bef_birth) throws Exception {
+    public int updateUserBefBirth(String id, String birth) throws Exception {
         Map<String, Object> map = new HashMap<>();
-        map.put("id", id);
-        map.put("bef_birth", bef_birth);
+        map.put("id", id); // 변경될 회원 ID
+        map.put("birth", birth);
         return session.update(namespace + "updateUserBefBirth", map);
     }
 
-    // 5.3. 핸드폰 번호 변경
+    // 핸드폰 번호 변경 (UPDATE)
     @Override
-    public int updateUserMobileNum(String id, Integer mobile_num) throws Exception {
+    public int updateUserMobileNum(String id, String ph_num) throws Exception {
         Map<String, Object> map = new HashMap<>();
-        map.put("id", id);
-        map.put("mobile_num", mobile_num);
+        map.put("id", id); // 변경될 회원 ID
+        map.put("ph_num", ph_num); // 변경할 핸드폰 번호
         return session.update(namespace + "updateUserMobileNum", map);
     }
 
-    // UPDATE
-    // 6. 회원탈퇴
-    // 6.1. 회원가입 상태를 Y -> N 으로 변경
+    // 회원탈퇴 (UPDATE)
+    // > 회원 상태를 'O' -> 'L' 으로 변경
     @Override
     public int updateUserState(String id) throws Exception {
         return session.update(namespace + "updateUserState", id);
     }
-
 }
--- src/main/java/com/firstSpring/domain/user/UserDto.java
+++ src/main/java/com/firstSpring/domain/user/UserDto.java
@@ -1,450 +1,390 @@
 package com.firstSpring.domain.user;
 
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 public class UserDto {
+    @NotBlank(message = "아이디는 필수 입력값입니다.")
+    @Pattern(regexp = "^[a-z0-9]{4,20}$", message = "아이디는 영문 소문자와 숫자 4~12자리여야 합니다.")
     @JsonProperty("id")
-    private final String id; // 회원ID
+    private String id; // 회원ID
 
     @JsonProperty("grd_cd")
-    private final String grdCd; // 회원등급코드
+    private String grdCd; // 회원등급코드
 
     @JsonProperty("st_cd")
-    private final String stCd; // 회원상태코드
+    private String stCd; // 회원상태코드
 
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "유효한 이메일 주소여야 합니다.")
     @JsonProperty("email")
-    private final String email; // 이메일
+    private String email; // 이메일
 
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
     @JsonProperty("pwd")
-    private final String pwd; // 비밀번호
+    private String pwd; // 비밀번호
 
     @JsonProperty("name")
-    private final String name; // 이름
+    private String name; // 이름
 
     @JsonProperty("birth")
-    private final String birth; // 생년월일
+    private String birth; // 생년월일
 
     @JsonProperty("gender")
-    private final char gender; // 성별
+    private String gender; // 성별
 
     @JsonProperty("ph_num")
-    private final String phNum; // 전화번호
+    private String phNum; // 전화번호
 
     @JsonProperty("zip")
-    private final String zip; // 우편번호
+    private String zip; // 우편번호
 
     @JsonProperty("road_adr")
-    private final String roadAdr; // 도로명주소
+    private String roadAdr; // 도로명주소
 
     @JsonProperty("jibun_adr")
-    private final String jibunAdr; // 지번주소
+    private String jibunAdr; // 지번주소
 
     @JsonProperty("det_adr")
-    private final String detAdr; // 상세주소
+    private String detAdr; // 상세주소
 
     @JsonProperty("is_mail_auth")
-    private final char isMailAuth; // 메일 인증 여부
+    private String isMailAuth; // 메일 인증 여부
 
     @JsonProperty("mail_key")
-    private final String mailKey; // 메일 인증 번호
+    private String mailKey; // 메일 인증 번호
 
     @JsonProperty("is_adm")
-    private final char isAdm; // 관리자여부
+    private String isAdm; // 관리자여부
 
     @JsonProperty("reg_dt")
-    private final String regDt; // 가입일
+    private String regDt; // 가입일
 
     @JsonProperty("quit_dt")
-    private final String quitDt; // 탈퇴일
+    private String quitDt; // 탈퇴일
 
     @JsonProperty("is_quit")
-    private final char isQuit; // 탈퇴여부
+    private String isQuit; // 탈퇴여부
 
     @JsonProperty("is_blok")
-    private final char isBlok; // 차단여부
+    private String isBlok; // 차단여부
 
     @JsonProperty("blok_res")
-    private final String blokRes; // 차단사유
+    private String blokRes; // 차단사유
 
     @JsonProperty("blok_dt")
-    private final String blokDt; // 차단날짜
+    private String blokDt; // 차단날짜
 
     @JsonProperty("un_blok_dt")
-    private final String unBlokDt; // 차단해지날짜
+    private String unBlokDt; // 차단해지날짜
 
     @JsonProperty("frst_reg_dt")
-    private final String frstRegDt; // 최초등록일시
+    private String frstRegDt; // 최초등록일시
 
     @JsonProperty("frst_reg_id")
-    private final String frstRegId; // 최종등록자식별자번호
+    private String frstRegId; // 최종등록자식별자번호
 
     @JsonProperty("last_mod_dt")
-    private final String lastModDt; // 최종수정일시
+    private String lastModDt; // 최종수정일시
 
     @JsonProperty("last_mod_id")
-    private final String lastModId; // 최종수정자식별번호
+    private String lastModId; // 최종수정자식별번호
 
-    private UserDto(Builder builder) {
-        this.id = builder.id;
-        this.grdCd = builder.grdCd;
-        this.stCd = builder.stCd;
-        this.email = builder.email;
-        this.pwd = builder.pwd;
-        this.name = builder.name;
-        this.birth = builder.birth;
-        this.gender = builder.gender;
-        this.phNum = builder.phNum;
-        this.zip = builder.zip;
-        this.roadAdr = builder.roadAdr;
-        this.jibunAdr = builder.jibunAdr;
-        this.detAdr = builder.detAdr;
-        this.isMailAuth = builder.isMailAuth;
-        this.mailKey = builder.mailKey;
-        this.isAdm = builder.isAdm;
-        this.regDt = builder.regDt;
-        this.quitDt = builder.quitDt;
-        this.isQuit = builder.isQuit;
-        this.isBlok = builder.isBlok;
-        this.blokRes = builder.blokRes;
-        this.blokDt = builder.blokDt;
-        this.unBlokDt = builder.unBlokDt;
-        this.frstRegDt = builder.frstRegDt;
-        this.frstRegId = builder.frstRegId;
-        this.lastModDt = builder.lastModDt;
-        this.lastModId = builder.lastModId;
+    @JsonProperty("birthYear")
+    private String birthYear;
+
+    @JsonProperty("birthMonth")
+    private String birthMonth;
+
+    @JsonProperty("birthDay")
+    private String birthDay;
+
+    // 기본 생성자
+    public UserDto() {
     }
 
-    public static class Builder {
-        private String id;
-        private String grdCd;
-        private String stCd;
-        private String email;
-        private String pwd;
-        private String name;
-        private String nick;
-        private String birth;
-        private char gender;
-        private String phNum;
-        private String zip;
-        private String divAdr;
-        private String roadAdr;
-        private String jibunAdr;
-        private String detAdr;
-        private char isMailAuth;
-        private String mailKey;
-        private char isAdm;
-        private String regDt;
-        private String quitDt;
-        private char isQuit;
-        private char isBlok;
-        private String blokRes;
-        private String blokDt;
-        private String unBlokDt;
-        private String frstRegDt;
-        private String frstRegId;
-        private String lastModDt;
-        private String lastModId;
-
-        public Builder id(String id) {
-            this.id = id;
-            return this;
-        }
-
-        public Builder grdCd(String grdCd) {
-            this.grdCd = grdCd;
-            return this;
-        }
-
-        public Builder stCd(String stCd) {
-            this.stCd = stCd;
-            return this;
-        }
-
-        public Builder email(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public Builder pwd(String pwd) {
-            this.pwd = pwd;
-            return this;
-        }
-
-        public Builder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Builder nick(String nick) {
-            this.nick = nick;
-            return this;
-        }
-
-        public Builder birth(String birth) {
-            this.birth = birth;
-            return this;
-        }
-
-        public Builder gender(char gender) {
-            this.gender = gender;
-            return this;
-        }
-
-        public Builder phNum(String phNum) {
-            this.phNum = phNum;
-            return this;
-        }
-
-        public Builder zip(String zip) {
-            this.zip = zip;
-            return this;
-        }
-
-        public Builder divAdr(String divAdr) {
-            this.divAdr = divAdr;
-            return this;
-        }
-
-        public Builder roadAdr(String roadAdr) {
-            this.roadAdr = roadAdr;
-            return this;
-        }
-
-        public Builder jibunAdr(String jibunAdr) {
-            this.jibunAdr = jibunAdr;
-            return this;
-        }
-
-        public Builder detAdr(String detAdr) {
-            this.detAdr = detAdr;
-            return this;
-        }
-
-        public Builder isMailAuth(char isMailAuth) {
-            this.isMailAuth = isMailAuth;
-            return this;
-        }
-
-        public Builder mailKey(String mailKey) {
-            this.mailKey = mailKey;
-            return this;
-        }
-
-        public Builder isAdm(char isAdm) {
-            this.isAdm = isAdm;
-            return this;
-        }
-
-        public Builder regDt(String regDt) {
-            this.regDt = regDt;
-            return this;
-        }
-
-        public Builder quitDt(String quitDt) {
-            this.quitDt = quitDt;
-            return this;
-        }
-
-        public Builder isQuit(char isQuit) {
-            this.isQuit = isQuit;
-            return this;
-        }
-
-        public Builder isBlok(char isBlok) {
-            this.isBlok = isBlok;
-            return this;
-        }
-
-        public Builder blokRes(String blokRes) {
-            this.blokRes = blokRes;
-            return this;
-        }
-
-        public Builder blokDt(String blokDt) {
-            this.blokDt = blokDt;
-            return this;
-        }
-
-        public Builder unBlokDt(String unBlokDt) {
-            this.unBlokDt = unBlokDt;
-            return this;
-        }
-
-        public Builder frstRegDt(String frstRegDt) {
-            this.frstRegDt = frstRegDt;
-            return this;
-        }
-
-        public Builder frstRegId(String frstRegId) {
-            this.frstRegId = frstRegId;
-            return this;
-        }
-
-        public Builder lastModDt(String lastModDt) {
-            this.lastModDt = lastModDt;
-            return this;
-        }
-
-        public Builder lastModId(String lastModId) {
-            this.lastModId = lastModId;
-            return this;
-        }
-
-        public Builder birth(String birthYear, String birthMonth, String birthDay) {
-            // 생년월일을 'YYYY-MM-DD' 형식으로 조합
-            this.birth = String.format("%04d-%02d-%02d", Integer.parseInt(birthYear), Integer.parseInt(birthMonth), Integer.parseInt(birthDay));
-            return this;
-        }
-
-        public UserDto build() {
-            return new UserDto(this);
-        }
-
-        public Builder from(UserDto customer) {
-            this.id = customer.id;
-            this.grdCd = customer.grdCd;
-            this.stCd = customer.stCd;
-            this.email = customer.email;
-            this.pwd = customer.pwd;
-            this.name = customer.name;
-            this.birth = customer.birth;
-            this.gender = customer.gender;
-            this.phNum = customer.phNum;
-            this.zip = customer.zip;
-            this.roadAdr = customer.roadAdr;
-            this.jibunAdr = customer.jibunAdr;
-            this.detAdr = customer.detAdr;
-            this.isMailAuth = customer.isMailAuth;
-            this.mailKey = customer.mailKey;
-            this.isAdm = customer.isAdm;
-            this.regDt = customer.regDt;
-            this.quitDt = customer.quitDt;
-            this.isQuit = customer.isQuit;
-            this.isBlok = customer.isBlok;
-            this.blokRes = customer.blokRes;
-            this.blokDt = customer.blokDt;
-            this.unBlokDt = customer.unBlokDt;
-            this.frstRegDt = customer.frstRegDt;
-            this.frstRegId = customer.frstRegId;
-            this.lastModDt = customer.lastModDt;
-            this.lastModId = customer.lastModId;
-            return this;
-        }
+    // 회원 정보 조회시 사용할 생성자
+    public UserDto(String id, String stCd, String email, String pwd, String name, String birth, String gender, String phNum, String mailKey, String regDt) {
+        this.id = id;
+        this.stCd = stCd;
+        this.email = email;
+        this.pwd = pwd;
+        this.name = name;
+        this.birth = birth;
+        this.gender = gender;
+        this.phNum = phNum;
+        this.mailKey = mailKey;
+        this.regDt = regDt;
     }
 
-    // Getters
+    // 회원가입시 사용할 생성자
+    public UserDto(String id, String email, String pwd, String name, String birth,
+                   String gender, String phNum, String zip, String roadAdr, String jibunAdr, String detAdr,
+                   String mailKey) {
+        this.id = id;
+        this.email = email;
+        this.pwd = pwd;
+        this.name = name;
+        this.birth = birth;
+        this.gender = gender;
+        this.phNum = phNum;
+        this.zip = zip;
+        this.roadAdr = roadAdr;
+        this.jibunAdr = jibunAdr;
+        this.detAdr = detAdr;
+        this.mailKey = mailKey;
+    }
+
+    public void setBirth(String birthYear, String birthMonth, String birthDay) {
+        this.birth = birthYear + "-" + birthMonth + "-" + birthDay;
+    }
+    public void setBirthYear(String birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public void setBirthMonth(String birthMonth) {
+        this.birthMonth = birthMonth;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    public String getBirthMonth() {
+        return birthMonth;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
     public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getGrdCd() {
         return grdCd;
     }
 
+    public void setGrdCd(String grdCd) {
+        this.grdCd = grdCd;
+    }
+
     public String getStCd() {
         return stCd;
+    }
+
+    public void setStCd(String stCd) {
+        this.stCd = stCd;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPwd() {
         return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getBirth() {
         return birth;
     }
 
-    public char getGender() {
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getGender() {
         return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 
     public String getPhNum() {
         return phNum;
     }
 
+    public void setPhNum(String phNum) {
+        this.phNum = phNum;
+    }
+
     public String getZip() {
         return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
     }
 
     public String getRoadAdr() {
         return roadAdr;
     }
 
+    public void setRoadAdr(String roadAdr) {
+        this.roadAdr = roadAdr;
+    }
+
     public String getJibunAdr() {
         return jibunAdr;
+    }
+
+    public void setJibunAdr(String jibunAdr) {
+        this.jibunAdr = jibunAdr;
     }
 
     public String getDetAdr() {
         return detAdr;
     }
 
-    public char getIsMailAuth() {
+    public void setDetAdr(String detAdr) {
+        this.detAdr = detAdr;
+    }
+
+    public String getIsMailAuth() {
         return isMailAuth;
+    }
+
+    public void setIsMailAuth(String isMailAuth) {
+        this.isMailAuth = isMailAuth;
     }
 
     public String getMailKey() {
         return mailKey;
     }
 
-    public char getIsAdm() {
+    public void setMailKey(String mailKey) {
+        this.mailKey = mailKey;
+    }
+
+    public String getIsAdm() {
         return isAdm;
+    }
+
+    public void setIsAdm(String isAdm) {
+        this.isAdm = isAdm;
     }
 
     public String getRegDt() {
         return regDt;
     }
 
+    public void setRegDt(String regDt) {
+        this.regDt = regDt;
+    }
+
     public String getQuitDt() {
         return quitDt;
     }
 
-    public char getIsQuit() {
+    public void setQuitDt(String quitDt) {
+        this.quitDt = quitDt;
+    }
+
+    public String getIsQuit() {
         return isQuit;
     }
 
-    public char getIsBlok() {
+    public void setIsQuit(String isQuit) {
+        this.isQuit = isQuit;
+    }
+
+    public String getIsBlok() {
         return isBlok;
+    }
+
+    public void setIsBlok(String isBlok) {
+        this.isBlok = isBlok;
     }
 
     public String getBlokRes() {
         return blokRes;
     }
 
+    public void setBlokRes(String blokRes) {
+        this.blokRes = blokRes;
+    }
+
     public String getBlokDt() {
         return blokDt;
+    }
+
+    public void setBlokDt(String blokDt) {
+        this.blokDt = blokDt;
     }
 
     public String getUnBlokDt() {
         return unBlokDt;
     }
 
+    public void setUnBlokDt(String unBlokDt) {
+        this.unBlokDt = unBlokDt;
+    }
+
     public String getFrstRegDt() {
         return frstRegDt;
+    }
+
+    public void setFrstRegDt(String frstRegDt) {
+        this.frstRegDt = frstRegDt;
     }
 
     public String getFrstRegId() {
         return frstRegId;
     }
 
+    public void setFrstRegId(String frstRegId) {
+        this.frstRegId = frstRegId;
+    }
+
     public String getLastModDt() {
         return lastModDt;
+    }
+
+    public void setLastModDt(String lastModDt) {
+        this.lastModDt = lastModDt;
     }
 
     public String getLastModId() {
         return lastModId;
     }
 
+    public void setLastModId(String lastModId) {
+        this.lastModId = lastModId;
+    }
+
     @Override
     public String toString() {
-        return "CustomerDTO{" +
+        return "UserDto{" +
                 "id='" + id + '\'' +
                 ", grdCd='" + grdCd + '\'' +
                 ", stCd='" + stCd + '\'' +
@@ -452,19 +392,19 @@ public class UserDto {
                 ", pwd='" + pwd + '\'' +
                 ", name='" + name + '\'' +
                 ", birth='" + birth + '\'' +
-                ", gender=" + gender +
+                ", gender='" + gender + '\'' +
                 ", phNum='" + phNum + '\'' +
                 ", zip='" + zip + '\'' +
                 ", roadAdr='" + roadAdr + '\'' +
                 ", jibunAdr='" + jibunAdr + '\'' +
                 ", detAdr='" + detAdr + '\'' +
-                ", isMailAuth=" + isMailAuth +
+                ", isMailAuth='" + isMailAuth + '\'' +
                 ", mailKey='" + mailKey + '\'' +
-                ", isAdm=" + isAdm +
+                ", isAdm='" + isAdm + '\'' +
                 ", regDt='" + regDt + '\'' +
                 ", quitDt='" + quitDt + '\'' +
-                ", isQuit=" + isQuit +
-                ", isBlok=" + isBlok +
+                ", isQuit='" + isQuit + '\'' +
+                ", isBlok='" + isBlok + '\'' +
                 ", blokRes='" + blokRes + '\'' +
                 ", blokDt='" + blokDt + '\'' +
                 ", unBlokDt='" + unBlokDt + '\'' +
@@ -475,4 +415,3 @@ public class UserDto {
                 '}';
     }
 }
-
